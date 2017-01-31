3e48decb2a0e94d74906db3a2ed623cb65431f39
Tanawat Kusungnoen <arm_hotmall@outlook.com>
2017-01-31 09:44:28 +0700

    Resolve Issue
    - Delete Target folder
    - Edit code (between --> LIMIT, OFFSET)

--- user-service/src/main/java/demo/UserRepository.java
+++ user-service/src/main/java/demo/UserRepository.java
@@ -23,7 +23,7 @@ public class UserRepository {
     }
     @Transactional(readOnly = true)
     public List<User> findItemsPerPage(Long page, Long items){
-        return this.jdbcTemplate.query("Select * From USERS WHERE id between ? and ?",new Object[]{((page-1)*items)+1,(page-1)*items+items},new UserRowMapper());
+        return this.jdbcTemplate.query("Select * From USERS WHERE id LIMIT ? OFFSET ?",new Object[]{items,(page-1)*items},new UserRowMapper());
     }
     @Transactional
     public void save(User user) {
