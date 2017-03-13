e60fbbd355594b2b9b797e90459866538cefc1a8
Tanawat Kusungnoen <s7070025@kmitl.ac.th>
2017-03-13 21:21:11 +0700

    Fix Register Service

--- RegisterService/src/main/java/app/soa4/Controller/RegisterController.java
+++ RegisterService/src/main/java/app/soa4/Controller/RegisterController.java
@@ -2,11 +2,13 @@ package app.soa4.Controller;
 import java.util.List;
 
 import app.soa4.Modal.Account;
-import app.soa4.Modal.Account;
-import app.soa4.Modal.MatchingRepository;
+import app.soa4.Modal.RegisterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class RegisterController {
     @Autowired
-    private MatchingRepository matchingRepository;
+    private RegisterRepository registerRepository;
     
-    @RequestMapping("/regis")
-    public List<Account> getAccount(@RequestParam(value = "username") String username ,  @RequestParam(value = "email") String email){
-        return this.matchingRepository.checkAccount(username, email);
+    @RequestMapping(value = "/regis", method = RequestMethod.POST)
+    public ResponseEntity<?> registerAccount(@RequestParam(value = "username") String username , @RequestParam(value = "password") String password , @RequestParam(value = "email") String email){
+        if(this.registerRepository.checkAccount(username, password, email)){
+            this.registerRepository.createAccount(username, password, email);
+            return new ResponseEntity<>("Create account complete.", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("You can't create account", HttpStatus.OK);
+        }
     }
 }
