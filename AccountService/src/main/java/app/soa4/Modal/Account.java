de0d83130d4d622ad2279ff726c932bb3dc6216f
Patopatohoho <fenriclaw@gmail.com>
2017-03-13 17:04:07 +0700

    Revert "Upload with wrong user"
    This reverts commit 031616c3a2f7672951ef17246b2f1505c1471169.

---
+++ AccountService/src/main/java/app/soa4/Modal/Account.java
@@ -0,0 +1,129 @@
+package app.soa4.Modal;
+public class Account {
+    private long account_id;
+    private String account_email;
+    private String account_username;
+    private String account_password;
+    private String account_name;
+    private String account_lastname;
+    private Integer account_age;
+    private String account_sex;
+    private String account_sexual_taste;
+    private float account_latitude;
+    private float account_longtitude;
+    private String account_location;
+    private String account_img_profile_path;
+    private String account_descriptions;
+
+    public long getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(long account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getAccount_email() {
+        return account_email;
+    }
+
+    public void setAccount_email(String account_email) {
+        this.account_email = account_email;
+    }
+
+    public String getAccount_username() {
+        return account_username;
+    }
+
+    public void setAccount_username(String account_username) {
+        this.account_username = account_username;
+    }
+
+    public String getAccount_password() {
+        return account_password;
+    }
+
+    public void setAccount_password(String account_password) {
+        this.account_password = account_password;
+    }
+
+    public String getAccount_name() {
+        return account_name;
+    }
+
+    public void setAccount_name(String account_name) {
+        this.account_name = account_name;
+    }
+
+    public String getAccount_lastname() {
+        return account_lastname;
+    }
+
+    public void setAccount_lastname(String account_lastname) {
+        this.account_lastname = account_lastname;
+    }
+
+    public Integer getAccount_age() {
+        return account_age;
+    }
+
+    public void setAccount_age(Integer account_age) {
+        this.account_age = account_age;
+    }
+
+    public String getAccount_sex() {
+        return account_sex;
+    }
+
+    public void setAccount_sex(String account_sex) {
+        this.account_sex = account_sex;
+    }
+
+    public String getAccount_sexual_taste() {
+        return account_sexual_taste;
+    }
+
+    public void setAccount_sexual_taste(String account_sextual_taste) {
+        this.account_sexual_taste = account_sextual_taste;
+    }
+
+    public float getAccount_latitude() {
+        return account_latitude;
+    }
+
+    public void setAccount_latitude(float account_latitude) {
+        this.account_latitude = account_latitude;
+    }
+
+    public float getAccount_longtitude() {
+        return account_longtitude;
+    }
+
+    public void setAccount_longtitude(float account_longtitude) {
+        this.account_longtitude = account_longtitude;
+    }
+
+    public String getAccount_location() {
+        return account_location;
+    }
+
+    public void setAccount_location(String account_location) {
+        this.account_location = account_location;
+    }
+
+    public String getAccount_img_profile_path() {
+        return account_img_profile_path;
+    }
+
+    public void setAccount_img_profile_path(String account_img_profile_path) {
+        this.account_img_profile_path = account_img_profile_path;
+    }
+
+    public String getAccount_descriptions() {
+        return account_descriptions;
+    }
+
+    public void setAccount_descriptions(String account_descriptions) {
+        this.account_descriptions = account_descriptions;
+    }
+}
