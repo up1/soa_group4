49d84c7c5792ee89f911b0ed665e9bb8b90abb61
Kontammada <s.kontammada@gmail.com>
2017-03-13 22:49:38 +0700

    Edit ImageService
    add 'delete image'
    change @RequestParam in ImageController.java to @RequestBody

---
+++ ImageService/src/main/java/app/soa4/Modal/ImageCreate.java
@@ -0,0 +1,40 @@
+package app.soa4.Modal;
+
+public class ImageCreate {
+    String type;
+    String name;
+    String path;
+    long uid;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+}
