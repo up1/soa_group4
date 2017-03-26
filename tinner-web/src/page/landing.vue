032cf5af671d7c4b0e707bc8939d4f0f4712a41d
Thatchakon Jom-ud <s7070053@kmitl.ac.th>
2017-03-26 23:09:53 +0700

    Create Tinner Web Branch
    - Cli Vue
    - Route Vue

---
+++ tinner-web/src/page/landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Tinner Project</h1>
+    <h1>Powered By Vue.js | Spring Boot</h1>
+  </div>
+</template>
+<script>
+  export default{
+    name:'landing',
+    components:{
+    }
+  }
+</script>
+<style scope>
+  h1{
+    font-family: 'Josefin Sans', sans-serif;
+  }
+</style>
