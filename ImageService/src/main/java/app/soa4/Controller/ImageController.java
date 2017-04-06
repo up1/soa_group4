d6e1f7d13b4ddbdb651aa3ad848396d9dd5365fa
Kontammada <s.kontammada@gmail.com>
2017-04-06 23:25:24 +0700

    Azure

--- ImageService/src/main/java/app/soa4/Controller/ImageController.java
+++ ImageService/src/main/java/app/soa4/Controller/ImageController.java
@@ -1,16 +1,62 @@
 package app.soa4.Controller;
 
 import app.soa4.Modal.*;
+import app.soa4.Repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class ImageController {
     @Autowired
     private ImageRepository imageRepository;
+
+    @RequestMapping(value = {"/image/{imagetype}/AzureUpload"}, method = RequestMethod.POST)
+    public ResponseEntity<String> addImage(@PathVariable("imagetype") String imageType,
+                           @RequestParam("file") MultipartFile file,
+                           @RequestParam("imagename") String imageName,
+                           @RequestParam("uid1") long uid1,
+                           @RequestParam("uid2") long uid2) throws IOException{
+
+        String containerName = null;
+        if (imageType.equals("profile")){
+            containerName = "profile-image";
+        } else if (imageType.equals("chat")){
+            containerName = "chat-image";
+        } else {
+            return new ResponseEntity<String>("Error, image not added", HttpStatus.BAD_REQUEST);
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmssSSS").format(Calendar.getInstance().getTime());
+        String[] part = imageName.split("\\.");
+
+        AzureBlob azureBlob = new AzureBlob(containerName);
+        String imageUrl = azureBlob.uploadToAzureBlob(
+                file.getInputStream(),
+                uid1 + "," + uid2 + "_" + timeStamp + "." + part[part.length-1],
+                (int)file.getSize());
+
+        switch(imageType){
+            case "profile" :
+                imageRepository.addProfileImage(imageUrl, uid1);
+                return new ResponseEntity<String>("Image added", HttpStatus.OK);
+            case "chat" :
+                imageRepository.addChatImage(imageUrl, uid1, uid2);
+                return new ResponseEntity<String>("Image added", HttpStatus.OK);
+            default:
+                return new ResponseEntity<String>("Error, image not added", HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @RequestMapping(value = {"/image/profile/{imageid}"}, method = RequestMethod.GET)
     public ProfileImage getProfileImage(@PathVariable("imageid") long id){
@@ -19,17 +65,7 @@ public class ImageController {
 
     @RequestMapping(value = {"/image/chat/{imageid}"}, method = RequestMethod.GET)
     public ChatImage getChatImage(@PathVariable("imageid") long id){
-        return this.imageRepository.getChatImageById(id);
-    }
-
-    @RequestMapping(value = {"/image/profile"}, method = RequestMethod.POST, consumes = "application/json")
-    public String postProfileImage(@RequestBody ProfileImageCreate profileImageCreate){
-        return this.imageRepository.addProfileImage(profileImageCreate.getPath(), profileImageCreate.getUid());
-    }
-
-    @RequestMapping(value = {"/image/chat"}, method = RequestMethod.POST, consumes = "application/json")
-    public String postChatImage(@RequestBody ChatImageCreate chatImageCreate){
-        return this.imageRepository.addChatImage(chatImageCreate.getPath(), chatImageCreate.getUid_1(), chatImageCreate.getUid_2());
+       return this.imageRepository.getChatImageById(id);
     }
 
     @RequestMapping(value = {"/image/profile/delete"}, method = RequestMethod.DELETE, consumes = "application/json")
--- ImageService/src/main/java/app/soa4/Repository/ImageRepository.java
+++ ImageService/src/main/java/app/soa4/Repository/ImageRepository.java
@@ -1,5 +1,8 @@
-package app.soa4.Modal;
+package app.soa4.Repository;
 
+import app.soa4.Modal.ChatImage;
+import app.soa4.Exception.ImageNotFoundException;
+import app.soa4.Modal.ProfileImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
