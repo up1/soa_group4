a86b3f844e1d65fe4ab2bf93252ef1c2585bc046
patopatohoho <fenriclaw@gmail.com>
2017-04-16 17:34:21 +0700

    Account service

---
+++ AccountService/src/main/java/app/soa4/Controller/AccountController.java
@@ -0,0 +1,38 @@
+package app.soa4.Controller;
+
+import app.soa4.Modal.Account;
+import app.soa4.Modal.AccountRepository;
+import app.soa4.Modal.AccountToMatching;
+import app.soa4.Modal.Editdata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AccountController {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @RequestMapping(value = "/AccountProfile", method = RequestMethod.GET)
+    public Account getAccount(@RequestParam(value="id", defaultValue="2") int id){
+        return this.accountRepository.accountProfile((long) id);
+    }
+
+    @RequestMapping(value = "/AccountToMatching", method = RequestMethod.GET)
+    public AccountToMatching sentToMatching(@RequestParam(value="id", defaultValue="1") int id){
+        System.err.print(id);
+        return this.accountRepository.accountToMatching((long) id);
+    }
+
+    @RequestMapping(value = "/EditAccount", method = RequestMethod.PUT)
+    public String updateProfile(@RequestBody Editdata editdata){
+        return this.accountRepository.editProfile(editdata.getAccount_email(), editdata
+        .getAccount_password(),editdata.getAccount_name(), editdata.getAccount_lastname(),
+                editdata.getAccount_age(), editdata.getAccount_sex(), editdata.getAccount_sexual_taste(),
+                editdata.getAccount_location(), editdata.getAccount_descriptions(), editdata.getAccount_id() );
+    }
+
+
+
+}
