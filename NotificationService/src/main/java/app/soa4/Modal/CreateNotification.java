e24af0ba83f5f47115ced3a327a9b28d92427520
Achiraya Songput <AbsoluteAchiraya@gmail.com>
2017-04-25 01:07:38 +0700

    Notification-service

---
+++ NotificationService/src/main/java/app/soa4/Modal/CreateNotification.java
@@ -0,0 +1,26 @@
+package app.soa4.Modal;
+
+/**
+ * Created by ARMY on 4/16/2017.
+ */
+public class CreateNotification {
+
+    private int account_id1;
+    private int account_id2;
+
+    public int getAccount_id1() {
+        return account_id1;
+    }
+
+    public void setAccount_id1(int account_id1) {
+        this.account_id1 = account_id1;
+    }
+
+    public int getAccount_id2() {
+        return account_id2;
+    }
+
+    public void setAccount_id2(int account_id2) {
+        this.account_id2 = account_id2;
+    }
+}
