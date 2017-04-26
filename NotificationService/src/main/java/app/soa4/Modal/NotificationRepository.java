7579475b7170dd53b332e2e58624317609ed5747
Achiraya Songput <AbsoluteAchiraya@gmail.com>
2017-04-26 21:18:21 +0700

    Delete unused package

--- NotificationService/src/main/java/app/soa4/Modal/NotificationRepository.java
+++ NotificationService/src/main/java/app/soa4/Modal/NotificationRepository.java
@@ -1,6 +1,6 @@
 package app.soa4.Modal;
 
-import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
